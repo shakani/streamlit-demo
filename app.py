00c637292d7f0d50d49dc603d72e590736707c20
Sami Hakani <sami.hakani@gmail.com>
2023-11-06 10:34:41 -0500

    Added text input for stock ticker

--- app.py
+++ app.py
@@ -4,16 +4,19 @@ import plotter
 
 def app():
     """Define main application entry point."""
-    st.title("Minimal Streamlit Template")
+    st.title("Stock Picker")
     # And write the rest of the app inside this function!
-    st.markdown("Here is where you can insert more text.")
-    st.markdown("""Streamlit works more on the basis of 
-\"insert things and let Streamlit figure out how to render 
-them\".  If you want more flexibility for how to render
-the markdown and other elements on your webpage, 
-you may want to consider
-[Flask](https://flask.palletsprojects.com/).""")
-    st.markdown("Here's another line of text")
+    st.markdown("Enter a stock ticker to see its closing price data for the last 100 days.")
+#     st.markdown("""Streamlit works more on the basis of 
+# \"insert things and let Streamlit figure out how to render 
+# them\".  If you want more flexibility for how to render
+# the markdown and other elements on your webpage, 
+# you may want to consider
+# [Flask](https://flask.palletsprojects.com/).""")
+    # st.markdown("Here's another line of text")
+    stock_ticker = st.text_input('Stock Ticker (e.g. $APPL)', '$APPL')
+    st.markdown(f'The stock you entered was {stock_ticker}')
+
     st.pyplot(plotter.plot.get_figure())
 
     st.sidebar.markdown("""You can put things in the sidebar:
