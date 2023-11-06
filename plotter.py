a987e2a42460018d31097f37e383c383b3c1c3cf
Sami Hakani <sami.hakani@gmail.com>
2023-11-06 09:14:45 -0500

    Used seaborn to plot TDI tutorial data

--- plotter.py
+++ plotter.py
@@ -1,4 +1,12 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 import requests 
+import streamlit as st
+
+df = pd.read_csv('question5.csv')
+print(df.head)
+
+plt.yscale('log')
+plot = sns.scatterplot(data=df.query('`Job created` <= 50'), x='Job created', y='Total Savings')
