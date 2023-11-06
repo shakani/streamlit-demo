833f08132284b866dbecdaef13289732bfa858fd
Sami Hakani <sami.hakani@gmail.com>
2023-11-06 13:22:09 -0500

    Added text to inform user about sidebar

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def app():
 
     st.title("Stock Picker")
     st.markdown("Enter a stock ticker to see its closing price data for the last 100 days.")
+    st.markdown("Sample data for IBM is shown below. API calls are limited to 25 per day. Please check the box in the sidebar on the left to make an API call.")
     stock_ticker = st.text_input('Stock Ticker (e.g. $IBM)', '$IBM')
 
     st.markdown(f'The stock you entered was {stock_ticker}')
