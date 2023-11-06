499c424a9184abeb0fdc19df0a495200bd34a775
Sami Hakani <sami.hakani@gmail.com>
2023-11-06 13:09:04 -0500

    Added checkbox to use API

--- app.py
+++ app.py
@@ -4,27 +4,21 @@ import plotter
 
 def app():
     """Define main application entry point."""
+    ### SIDEBAR ###
+    use_API = st.sidebar.checkbox('Use API?', value=False)
+
     st.title("Stock Picker")
-    # And write the rest of the app inside this function!
     st.markdown("Enter a stock ticker to see its closing price data for the last 100 days.")
-#     st.markdown("""Streamlit works more on the basis of 
-# \"insert things and let Streamlit figure out how to render 
-# them\".  If you want more flexibility for how to render
-# the markdown and other elements on your webpage, 
-# you may want to consider
-# [Flask](https://flask.palletsprojects.com/).""")
-    # st.markdown("Here's another line of text")
-
     stock_ticker = st.text_input('Stock Ticker (e.g. $IBM)', '$IBM')
+
     st.markdown(f'The stock you entered was {stock_ticker}')
-
-    plot = plotter.get_stock_plot(stock_ticker, use_API=False)
-
+    plot = plotter.get_stock_plot(stock_ticker, use_API=use_API)
     st.pyplot(plot.get_figure())
+    
 
-    st.sidebar.markdown("""You can put things in the sidebar:
-* Like lists
-* Or other things""")
+    #st.sidebar.markdown("""You can put things in the sidebar:
+# * Like lists
+# * Or other things""")
 
 if __name__ == '__main__':
     app()
